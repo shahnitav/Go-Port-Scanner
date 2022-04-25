f872ccfb904b2ee01c0f0126e8555c4fbc57f6c7
Nitav Shah <shahnitav@gmail.com>
2022-04-25 02:54:10 -0400

    Commit: ICMP Scan

--- go.mod
+++ go.mod
@@ -2,11 +2,20 @@ module github.com/shahnitav/Go-Port-Scanner
 
 go 1.18
 
-require github.com/akamensky/argparse v1.3.1
+require (
+	github.com/akamensky/argparse v1.3.1
+	github.com/fatih/color v1.13.0
+)
 
 require (
-	github.com/fatih/color v1.13.0 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	golang.org/x/net v0.0.0-20220421235706-1d1ef9303861 // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+)
+
+require (
+	github.com/go-ping/ping v0.0.0-20211130115550-779d1e919534
 	github.com/mattn/go-colorable v0.1.9 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
-	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+	golang.org/x/sys v0.0.0-20220422013727-9388b58f7150 // indirect
 )
--- main.go
+++ main.go
@@ -10,13 +10,17 @@ import (
 	"strconv"
 	"sync"
 	"github.com/fatih/color"
+	"encoding/binary"
+	"github.com/go-ping/ping"
+	"runtime"
 )
 
-//Declare global variables for the IP Address, Port Range and whether it is a SYN Scan
-var ip string
+//Declare global variables for the IP Address, Port Range and whether it is a ICMP Scan
+var argIP *string
+var ip []string
 var ports []string
 var timeout int
-var SYN bool
+var ICMP bool
 
 // Common Ports to give description to port numbers
 var commonPorts = map[int] string {
@@ -64,7 +68,108 @@ type PortScan struct {
 	IsOpen bool //Status
 }
 
-var result []PortScan // Results
+type HostScan struct {
+	IP string //IP Address of Host
+ 	IsUP bool //Status
+}
+
+var result []PortScan // Port Scan Results
+var resultHost []HostScan // Host Scan Results
+
+//Ping an IP Address to check if its UP
+func pingFunc(pingIP string) HostScan {
+	res := &HostScan {
+		IP: pingIP,
+		IsUP: false,
+	}
+	pinger, err:= ping.NewPinger(pingIP)
+	pinger.SetPrivileged(true)
+	if err != nil {
+		fmt.Println("Fatal Error while sending an ICMP packet to ", pingIP)
+		os.Exit(1)
+	}
+	pinger.Count = 1
+	pinger.Timeout = time.Millisecond * 500
+	pinger.OnRecv  = func(pkt *ping.Packet) {
+		res.IsUP = true
+	}
+	err = pinger.Run()
+	if err != nil {
+		fmt.Println("Fatal Error while sending an ICMP packet to ", pingIP)
+		os.Exit(1)
+	}
+	return *res
+}
+
+//Checks if the host is up
+func isHostUp(ip []string) {
+		// Check for root privileges in Linux required to send raw ICMP Packets
+		if runtime.GOOS == "linux" && os.Geteuid() != 0 {
+			fmt.Printf("To do a host scan on Linux, you need Admin privileges. Run the binary with 'sudo'.")
+			os.Exit(1)
+		} else {
+			ch := make(chan HostScan)
+			//Launch goroutine
+			go func() {
+				var wg sync.WaitGroup
+				wg.Add(len(ip))
+				fmt.Println("IP Len - ", len(ip))
+				for _, ip_elem := range ip {
+					go func(ip_elem string) {
+						defer wg.Done()
+						res := pingFunc(ip_elem)
+						if res.IsUP {
+							ch <- res
+						}
+					}(ip_elem)
+				}
+				wg.Wait()
+				close(ch)	
+			}()
+			//Append status updates over channel to resultHost
+			for elem := range ch {
+				resultHost = append(resultHost, elem)
+			}
+		}
+}
+
+//Parse CIDR IP Address to get a list of IP Addresses
+func getIP(argIP string) []string {
+	// convert string to IPNet struct
+	_, ipv4Net, err := net.ParseCIDR(argIP)
+	if err != nil {
+		fmt.Println("Fatal error while parsing IP Address: ", argIP)
+		os.Exit(1)
+	}
+	// convert IPNet struct mask and address to uint32
+	mask := binary.BigEndian.Uint32(ipv4Net.Mask)
+	// find the start IP address
+	start := binary.BigEndian.Uint32(ipv4Net.IP)
+	// find the final IP address
+	finish := (start & mask) | (mask ^ 0xffffffff)
+	// make a slice to return host addresses
+	var hosts []string
+	// loop through addresses as uint32.
+    // I used "start + 1" and "finish - 1" to discard the network and broadcast addresses.
+	for i := start + 1; i <= finish-1; i++ {
+		// convert back to net.IPs
+        // Create IP address of type net.IP. IPv4 is 4 bytes, IPv6 is 16 bytes.
+		ip := make(net.IP, 4)
+		binary.BigEndian.PutUint32(ip, i)
+		hosts = append(hosts, ip.String())
+	}
+	// return a slice of strings containing IP addresses
+	return hosts
+}
+
+//Print the result
+func printHostResult(elapsed time.Duration){
+	color.Cyan("Host Scan Results")
+	for _, elem := range resultHost {
+		color.Green("IP %s %v\n", elem.IP, elem.IsUP)
+	}
+	fmt.Printf("Scan duration - %s", elapsed)
+}
 
 //Connects to a port to check its status
 func connect(ip string , port int, timeout int) PortScan{
@@ -81,7 +186,7 @@ func connect(ip string , port int, timeout int) PortScan{
 }
 
 //Check if the TCP Port is open 
-func isPortOpen(ports []string) []PortScan {
+func isPortOpen(ports []string) {
 	ch := make(chan PortScan)
 	//Launch goroutine
 	go func() {
@@ -95,7 +200,7 @@ func isPortOpen(ports []string) []PortScan {
 			}
 			go func(port_int int) {
 				defer wg.Done()
-				res := connect(ip, port_int, timeout)
+				res := connect(ip[0], port_int, timeout)
 				if res.IsOpen {
 					ch <- res
 				}
@@ -109,13 +214,11 @@ func isPortOpen(ports []string) []PortScan {
 	for elem := range ch {
 		result = append(result, elem)
 	}
-
-	return result
 }
 
 //Print the result
 func printResult(elapsed time.Duration){
-	color.Cyan("Results")
+	color.Cyan("Port Scan Results")
 	for _, elem := range result {
 		desc, ok := commonPorts[elem.Port]
 		if ok {
@@ -173,25 +276,30 @@ func getPortList(argPort string) []string {
 }
 
 // Argument parser and validator
-func parseArgs() (string, []string, int, bool) {
+func parseArgs() ([]string, []string, int, bool) {
 	parser := argparse.NewParser("Go Port Scanner", "Scans for Open Ports on the given IP Address and Range of Ports.")
 	//Create IP Flag
-	ip := parser.String("", "ip", &argparse.Options{Required: true, Help: "Target IP"})
+	argIP = parser.String("", "ip", &argparse.Options{Required: true, Help: "Target IP address with support for CIDR eg. 192.168.126.131/28"})
 	//Create Port Flag
-	argPort := parser.String("p", "port", &argparse.Options{Required: true, Help: "Ports to scan, can be a single value 80, or a range 80-120, or multiple values 80,91,443"})
+	argPort := parser.String("p", "port", &argparse.Options{Required: false, Help: "Ports to scan, can be a single value 80, or a range 80-120, or multiple values 80,91,443"})
 	//Create Timeout Flag
 	timeout := parser.Int("", "t", &argparse.Options{Required: false, Help: "Timeout in Millisecond, Default -> 500ms", Default: 500})
 	//Create SYN Flag
-	SYN := parser.Flag("s", "syn", &argparse.Options{Required: false, Help: "Will perform a SYN stealth scan to check for open ports"})
+	ICMP := parser.Flag("i", "icmp", &argparse.Options{Required: false, Help: "Run an ICMP scan to check on live hosts"})
 	parser.Parse(os.Args)
 	//Validate Arguments
-	if net.ParseIP(*ip) == nil {
-		fmt.Println("IP Address: "+ *ip + "is invalid")
-		fmt.Println(os.Args[0] + " -h for Help")
-		os.Exit(0)
+	//If CIDR get a list of IP Addresses
+	if strings.Contains(*argIP, "/") || *ICMP {
+		ip = getIP(*argIP)
+	} else {
+		if net.ParseIP(*argIP) == nil {
+			fmt.Println("IP Address: "+ *argIP + "is invalid")
+			fmt.Println(os.Args[0] + " -h for Help")
+			os.Exit(0)
+		}
+		ip = []string{*argIP}
+		ports = getPortList(*argPort)
 	}
-	ports := getPortList(*argPort)
-
 	// Replace parser.Usage as the help message
 	parser.HelpFunc = func(c *argparse.Command, msg interface{}) string {
 		var help string
@@ -206,15 +314,21 @@ func parseArgs() (string, []string, int, bool) {
 		return help
 	}
 
-	return *ip, ports, *timeout, *SYN
+	return ip, ports, *timeout, *ICMP
 }
 
 func main() {
 	color.Cyan("Go Port Scanner")
-	ip, ports, timeout, SYN = parseArgs()
+	ip, ports, timeout, ICMP = parseArgs()
 	start := time.Now()
-	fmt.Println("IP: ", ip, "\nTimeout: ", timeout, "ms \nSYN: ", SYN)
-	isPortOpen(ports)
-	elapsed := time.Since(start)
-	printResult(elapsed)
+	fmt.Println("IP: ", *argIP, "\nTimeout: ", timeout, "ms \nICMP: ", ICMP)
+	if ICMP {
+		isHostUp(ip)
+		elapsed := time.Since(start)
+		printHostResult(elapsed)
+	} else {
+		isPortOpen(ports)
+		elapsed := time.Since(start)
+		printResult(elapsed)
+	}
 }
